6e93dd7d613213ca0681e3d7ff3c498cf7a7f2f8
leunggamciu <gamciuleung@gmail.com>
2014-08-13 09:40:54 +0800

    为mainproduct模块添加中文翻译

---
+++ upload/catalog/language/chinese/module/mainproduct.php
@@ -0,0 +1,8 @@
+<?php
+
+$_['text_general']    = '概述';
+$_['text_function']   = '功能';
+$_['text_design']     = '设计';
+$_['text_attribute']  = '规格参数';
+
+?>
---
+++ upload/catalog/language/english/module/mainproduct.php
@@ -0,0 +1,8 @@
+<?php
+
+$_['text_general']   = 'General';
+$_['text_function']  = 'Function';
+$_['text_design']    = 'Design';
+$_['text_attribute'] = 'Attribute';
+
+?>
