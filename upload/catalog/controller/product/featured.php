89985d4dcd628d8d3c75006a60ae1e07a86c400a
Jayin Ton <tonjayin@gmail.com>
2014-08-14 10:18:07 +0800

    特色商品展示与修改 close #10

---
+++ upload/catalog/controller/product/featured.php
@@ -0,0 +1,53 @@
+<?php
+class ControllerProductFeatured extends Controller{
+    //get produect  ..then var_dump it
+    public function Index(){
+        $this->load->model('catalog/product');
+        $this->load->model('tool/image');
+
+
+        $products = explode(',', $this->config->get('featured_product'));
+      
+        //featured_products
+        $this->data['featured_products'] = array();
+
+        foreach ($products as $product_id) {
+            $product_info = $this->model_catalog_product->getProduct($product_id);
+
+            if ($product_info) {
+                $this->data['featured_products'][] = array(
+                    'product_id' => $product_info['product_id'],
+                    'name'       => $product_info['name'],
+                    'image'      => $this->model_tool_image->resize($product_info['image'], $this->config->get('config_image_thumb_width'), $this->config->get('config_image_thumb_height')),
+                    'price'      => $product_info['price'],
+                    'href'       => $this->url->link('product/product', 'product_id=' . $product_info['product_id'])
+                );
+            }
+        }   
+        //all products
+        $this->data['all_products'] = array();
+        $all_products = $this->model_catalog_product->getProducts();
+        foreach ($all_products as $product) {
+              $this->data['all_products'][] = array(
+                    'product_id' => $product['product_id'],
+                    'name'       => $product['name'],
+                    'image'      => $this->model_tool_image->resize($product['image'], $this->config->get('config_image_thumb_width'), $this->config->get('config_image_thumb_height')),
+                    'price'      => $product['price'],
+                    'href'       => $this->url->link('product/product', 'product_id=' . $product['product_id'])
+                );
+        }
+
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/product/featured.tpl')) {
+            $this->template = $this->config->get('config_template') . '/template/product/featured.tpl';
+        } else {
+            $this->template = 'default/template/product/featured.tpl';
+        }
+
+        $this->children = array(
+            'common/header',
+            'common/footer'
+        );
+
+        $this->response->setOutput($this->render());
+    }
+}
---
+++ upload/catalog/view/theme/default/template/product/featured.tpl
@@ -0,0 +1,29 @@
+<?php echo $header;?>
+<link media="screen" href="catalog/view/css/phoneaccessories/list.css" type="text/css" rel="stylesheet">
+<section class="container clearfix" id="phaccsect1">
+        <h2 class="phacc_h2 f-2l"><div><span class="fl">精选配件</span></div></h2>
+        <ul class="phaccul_1">
+            <?php foreach ($featured_products as $product) { ?>
+                <li class="phaccul_li01">
+                    <div class="nobrdr nobrdb">
+                        <figure><a href="<?php echo $product['href'];?>"><img src="<?php echo $product['image'];?>" alt="<?php echo $product['name'];?>"></a></figure>
+                        <figcaption><a href="<?php echo $product['href'];?>"><?php echo $product['name'];?><em><?php echo $product['price'];?></em></a></figcaption>
+                    </div>
+                </li>
+            <?php }?>  
+        </ul>
+    </section>
+<section class="container clearfix" id="phaccsect2">
+        <h2 class="phacc_h2 f-2l"><div><span class="fl">全部配件</span></div></h2>
+         <ul class="phaccul_1">
+            <?php foreach ($all_products as $product) { ?>
+                <li class="phaccul_li01">
+                    <div class="nobrdr nobrdb">
+                        <figure><a href="<?php echo $product['href'];?>"><img src="<?php echo $product['image'];?>" alt="<?php echo $product['name'];?>"></a></figure>
+                        <figcaption><a href="<?php echo $product['href'];?>"><?php echo $product['name'];?><em><?php echo $product['price'];?></em></a></figcaption>
+                    </div>
+                </li>
+            <?php }?>  
+        </ul>
+</section>
+<?php echo $footer;?>
