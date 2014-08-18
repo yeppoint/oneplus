23bf9eb9c529e4e7c8cec03ffa0c7920b7cfb901
leunggamciu <gamciuleung@gmail.com>
2014-08-18 08:44:01 +0800

    增加header翻译

--- upload/catalog/language/chinese/common/header.php
+++ upload/catalog/language/chinese/common/header.php
@@ -10,9 +10,15 @@ $_['text_items']    		= '%s 个商品 - %s';
 $_['text_search']   		= '搜索';
 $_['text_welcome']  		= '欢迎光临，您可以<a href="%s">登录</a> 或 <a href="%s">注册一个帐户</a>。';
 $_['text_logged']   		= '您登录为<a href="%s">%s</a> <b>(</b> <a href="%s">退出</a> <b>)</b>';
+$_['text_login']            = '登录';
+$_['text_register']         = '注册';
+$_['text_cart']             = '购物车';
 $_['text_account']  		= '会员中心';
 $_['text_checkout'] 		= '去结账';
 $_['text_language'] 		= '语言';
 $_['text_currency'] 		= '货币';
 $_['text_clientservice']    = '客户服务';
+$_['text_overview']         = '产品浏览';
+$_['text_accessories']      = '产品配件';
+$_['text_support']          = '技术支持';
 ?>
